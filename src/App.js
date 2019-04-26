a200e8ca49774c67c2c6ee21cb40865ab181df13
GitRall <rasmus_olsson_89@hotmail.com>
2019-04-26 10:40:59 +0200

    react-router-dom and helmet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Login from './Components/Login';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path='/' component={Login}></Route>
+      </div>
+    </Router>
   );
 }
 
