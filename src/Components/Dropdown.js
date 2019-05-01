5d2d754afadd475968e305ed13f7219790a6f2c8
ultraemme <e.larsson1994@gmail.com>
2019-05-01 22:10:33 +0200

    dropdown styling

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -4,12 +4,12 @@ import styles from './Dropdown.module.css';
 const Dropdown = (props) => {
   return (
     <div className={styles.dropdown}>
-      <ul>
-        <li><span className={`material-icons`}>star</span>Add to favorites</li>
-        <li><span className={`material-icons`}>create</span>Rename</li>
-        <li><span className={`material-icons`}>file_copy</span>Copy</li>
-        <li><span className={`material-icons`}>exit_to_app</span>Move</li>
-        <li><span className={`material-icons`}>delete</span>Delete</li>
+      <ul className={styles.dropdown__list}>
+        <li className={styles['dropdown__list-item']}><span className={`${styles['dropdown__list-icon']} material-icons`}>star</span><span className={styles['dropdown__list-label']}>Add to favorites</span></li>
+        <li className={styles['dropdown__list-item']}><span className={`${styles['dropdown__list-icon']} material-icons`}>create</span><span className={styles['dropdown__list-label']}>Rename</span></li>
+        <li className={styles['dropdown__list-item']}><span className={`${styles['dropdown__list-icon']} material-icons`}>file_copy</span><span className={styles['dropdown__list-label']}>Copy</span></li>
+        <li className={styles['dropdown__list-item']}><span className={`${styles['dropdown__list-icon']} material-icons`}>exit_to_app</span><span className={styles['dropdown__list-label']}>Move</span></li>
+        <li className={styles['dropdown__list-item']}><span className={`${styles['dropdown__list-icon']} material-icons`}>delete</span><span className={styles['dropdown__list-label']}>Delete</span></li>
       </ul>
     </div>
   )
