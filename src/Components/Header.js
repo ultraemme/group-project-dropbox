cbd4b7993264d5c5d0b2fa34ef376b47da0cbf09
jonasoberg89 <folke_89@hotmail.com>
2019-05-01 17:04:26 +0200

    css i header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
-import React  from 'react';
+import React from 'react';
 import styles from './Header.module.css';
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 const Header = (props) => {
@@ -8,17 +8,22 @@ const Header = (props) => {
   let currentDir = "";
   return (
     <header className={styles.header}>
-      {
-        pathArr.map((path) => {
-          currentDir += `/${path}`;
-          return (
-          <span key={path}>
-            <Link className={styles['header__link']} to={currentDir}>{path}</Link>
-            <span className={styles['header__link-arrow']}> > </span>
-          </span>
-        )
-      })
-    }
+      {<div className={styles.header__container}>
+        {
+          pathArr.map((path) => {
+            currentDir += `/${path}`;
+            return (
+              <span key={path}>
+                <Link className={styles['header__link']} to={currentDir}>{path}</Link>
+                <span className={styles['header__link-arrow']}> > </span>
+              </span>
+            )
+          })
+        }
+
+      </div>
+
+      }
     </header>
   )
 }
