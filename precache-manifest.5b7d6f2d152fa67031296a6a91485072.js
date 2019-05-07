7b95bbc4cef38aa2df986cfe62a1d16f239cb76a
ultraemme <e.larsson1994@gmail.com>
2019-05-07 13:25:27 +0200

    Updates

---
+++ precache-manifest.5b7d6f2d152fa67031296a6a91485072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a5fa1ef6da0f2e015438f0809b981a5",
+    "url": "/mandatory-advanced-js5/index.html"
+  },
+  {
+    "revision": "82fba54c5bcd4ee04dc6",
+    "url": "/mandatory-advanced-js5/static/css/main.8043f37b.chunk.css"
+  },
+  {
+    "revision": "09062b958cdb3fca0cf5",
+    "url": "/mandatory-advanced-js5/static/js/2.c251956b.chunk.js"
+  },
+  {
+    "revision": "82fba54c5bcd4ee04dc6",
+    "url": "/mandatory-advanced-js5/static/js/main.043066d5.chunk.js"
+  },
+  {
+    "revision": "693ed353e3382e7179ea",
+    "url": "/mandatory-advanced-js5/static/js/runtime~main.d0f14806.js"
+  },
+  {
+    "revision": "ed84172e7fb61c339ffce1ee7b0e8c9a",
+    "url": "/mandatory-advanced-js5/static/media/lockbox_logo.ed84172e.svg"
+  }
+]);
