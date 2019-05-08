2723c667b84f92915e082bfb2bd8747023e75890
jonasoberg89 <folke_89@hotmail.com>
2019-05-08 13:43:32 +0200

    fix

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Dialog from './Components/Dialog';
+import {shallow, configure} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({adapter: new Adapter()})
+
+test('check onChange function', () => {
+  const wrapper = shallow (<Dialog />)
+  wrapper.find("input").simulate("change",{target:{ value :"test"}})
+  console.log (wrapper.state())
 });
