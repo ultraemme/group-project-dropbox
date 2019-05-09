08064d5d5c4ce1e454854c2dd2f6ccbd14e584dc
GitRall <rasmus_olsson_89@hotmail.com>
2019-05-09 14:02:04 +0200

    MoveFile Test

--- src/MoveFile.test.js
+++ src/MoveFile.test.js
@@ -5,3 +5,11 @@ import {shallow, configure} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 configure({ adapter: new Adapter() });
+
+
+it('Can render folder list', () => {
+
+  const wrapper = shallow(<MoveFile />);
+  let radioButtons = wrapper.find('input');
+  console.log(radioButtons);
+})
