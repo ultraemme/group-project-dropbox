d803c9717a3faef6d7a4912fb85d3d09a211c5a7
jonasoberg89 <folke_89@hotmail.com>
2019-05-09 13:32:14 +0200

    test not finish

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Dialog from './Components/Dialog';
 import {shallow, configure} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
