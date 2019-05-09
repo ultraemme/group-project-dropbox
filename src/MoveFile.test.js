3ab902c3166131d7c5d64db93b1e671960ab63bf
GitRall <rasmus_olsson_89@hotmail.com>
2019-05-09 10:18:56 +0200

    added enzyme as dev dep

---
+++ src/MoveFile.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MoveFile from './Components/MoveFile';
+import {shallow, configure} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
