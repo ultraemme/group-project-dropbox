31b9e85c79479a059382d31ebfab2e774c60f076
ultraemme <e.larsson1994@gmail.com>
2019-05-16 12:25:12 +0200

    Updates

--- precache-manifest.dd816ebaef96ccd4d090a8c93557c5c8.js
+++ precache-manifest.dd816ebaef96ccd4d090a8c93557c5c8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7953bc4743c5128cae8a58c004a5ce2d",
+    "revision": "4b9c759c78164969a05d93105b6f068b",
     "url": "/mandatory-advanced-js5/index.html"
   },
   {
-    "revision": "65a55f9199cd33c3bd98",
+    "revision": "efd3193528c53e677718",
     "url": "/mandatory-advanced-js5/static/css/main.36cb01d6.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/mandatory-advanced-js5/static/js/2.7c42f64a.chunk.js"
   },
   {
-    "revision": "65a55f9199cd33c3bd98",
-    "url": "/mandatory-advanced-js5/static/js/main.01cc806e.chunk.js"
+    "revision": "efd3193528c53e677718",
+    "url": "/mandatory-advanced-js5/static/js/main.b51732cf.chunk.js"
   },
   {
     "revision": "693ed353e3382e7179ea",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/mandatory-advanced-js5/precache-manifest.ad01dd28fe867fd75b87cd840b4400b4.js"
+  "/mandatory-advanced-js5/precache-manifest.dd816ebaef96ccd4d090a8c93557c5c8.js"
 );
 
 self.addEventListener('message', (event) => {
